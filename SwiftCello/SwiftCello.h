8ee4f4bd6acbe51bc47d705c75418b98f8a3609b
ilker cam <ilker.cam@isik.edu.tr>
2019-05-07 19:41:20 +0300

    Initial Commit

---
+++ SwiftCello/SwiftCello.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCello.h
+//  SwiftCello
+//
+//  Created by Ilker Cam on 7.05.2019.
+//  Copyright © 2019 Ilker Cam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCello.
+FOUNDATION_EXPORT double SwiftCelloVersionNumber;
+
+//! Project version string for SwiftCello.
+FOUNDATION_EXPORT const unsigned char SwiftCelloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCello/PublicHeader.h>
+
+
